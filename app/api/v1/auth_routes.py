502418f08b2dcdc13f2642571c5b894db3a7e5c3
Raju Kola <rajukola754@gmail.com>
2026-01-19 00:56:31 +0530

    added form data

--- app/api/v1/auth_routes.py
+++ app/api/v1/auth_routes.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter,Depends,HTTPException,status
+from fastapi import APIRouter,Depends
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
 
 from app.schemas.token_schema import Token
 from app.db.session import get_db
@@ -10,10 +11,10 @@ from app.core.errors import unauthorized,forbidden
 auth_router=APIRouter(prefix="/auth",tags=['auth'])
 
 @auth_router.post("/login",response_model=Token)
-def login(email:str,password:str,db:Session=Depends(get_db)):
-    user = db.query(UserModel).filter(UserModel.email==email).first()
+def login(form_data:OAuth2PasswordRequestForm=Depends(),db:Session=Depends(get_db)):
+    user = db.query(UserModel).filter(UserModel.email==form_data.username).first()
 
-    if not user or not verify_password(password,user.hashed_password):
+    if not user or not verify_password(form_data.password,user.hashed_password):
         unauthorized()
     if not user.is_active:
         forbidden("Inactive user")
